cb6429ea519704a2b412e5b6e187b737a8e21d69
정윤주 <oyunjooya@naver.com>
2025-09-29 01:07:33 +0900

    Feat: 3주차 실습(useEffect로 데이터 호출)

--- 윤-정윤주/3주차/실습/ReactRouter/src/App.tsx
+++ 윤-정윤주/3주차/실습/ReactRouter/src/App.tsx
@@ -1,35 +1,38 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+// 1. React Router에서 필요한 함수/컴포넌트를 import
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+import NotFound from './pages/not-found';
+import HomePage from './pages/home';
+import MoviesPage from './pages/movies';
+import RootLayOut from './layout/root-layout';
+
+// 2. createBrowserRouter로 라우터 생성
+const router = createBrowserRouter([
+  {
+    path: '/',
+    // element: <HomePage />,
+    element: <RootLayOut />,
+    errorElement: <NotFound />,
+    // Navbar 아래에 표시할 자식 라우트
+    children: [
+      {
+        index: true, // path 없이 기본으로 보여줄 페이지
+        element: <HomePage />
+      },
+      {
+        // 부모가 '/'이므로 '/movies'가 됨
+        path: 'movies',  // /movies/뒤에 오는 값을movieId로 받음
+        element: <MoviesPage />
+      },
+    ]
+  },
+]);
+
+// 3. RouterProvider로 router 전달
+function App() {
+  return <RouterProvider router={router} />
 }
 
-export default App
+export default App;
