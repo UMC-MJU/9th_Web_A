f1cedacf2d3e27a41ea7d0beadff65676f40eef4
eun <pdaeun0903@gmail.com>
2025-10-01 23:54:45 +0900

    FEAT: Navbar 컴포넌트 추가 및 라우팅 링크 구현

--- 은-박다은/3주차/mission-2/src/pages/HomePage.tsx
+++ 은-박다은/3주차/mission-2/src/pages/HomePage.tsx
@@ -1,8 +1,10 @@
 import { Outlet } from "react-router-dom";
+import { Navbar } from "../components/Navbar";
 
 export const HomePage = () => {
   return (
     <div>
+      <Navbar />
       <Outlet />
     </div>
   );
