9d65108d724f2bf42f3781526cbe25d7e3555bee
정윤주 <oyunjooya@naver.com>
2025-10-09 20:58:56 +0900

    Feat(미션3): 4주차 미션3

--- 윤-정윤주/4주차/chapter4-2/mission2-FE/src/App.tsx
+++ 윤-정윤주/4주차/chapter4-2/mission2-FE/src/App.tsx
@@ -5,6 +5,7 @@ import NotFoundPage from './pages/NotFoundPage'
 import LoginPage from './pages/LoginPage'
 import HomeLayout from './layouts/HomeLayout'
 import SignupPage from './pages/SignupPage'
+import MyPage from './pages/MyPage'
 
 const router = createBrowserRouter([
   {
@@ -15,6 +16,7 @@ const router = createBrowserRouter([
       { index: true, element: <HomePage />},  // "/"경로일 때 렌더링되는 기본 페이지
       { path: "login", element: <LoginPage /> },  // Outlet에 렌더링될 컴포넌트
       { path: "signup", element: <SignupPage /> },
+      { path: "my", element: <MyPage /> },
     ],
   },
 ]);
--- 윤-정윤주/4주차/chapter4-2/mission2-FE/src/pages/LoginPage.tsx
+++ 윤-정윤주/4주차/chapter4-2/mission2-FE/src/pages/LoginPage.tsx
@@ -1,7 +1,12 @@
+import { useNavigate } from "react-router-dom";
+import { postSignin } from "../apis/auth";
+import { LOCAL_STORAGE_KEY } from "../constants/key";
 import useForm from "../hooks/useForm";
+import { useLocalStorage } from "../hooks/useLocalStorage";
 import { vaildateSignin, type UserSigninInformation } from "../utils/validate";
 
 const LoginPage = () => {
+    const { setItem } = useLocalStorage(LOCAL_STORAGE_KEY.accessToken);
     const { values, errors, touched, getInputProps } = useForm<UserSigninInformation>({
         initialValues: {
             email: '', 
@@ -10,44 +15,65 @@ const LoginPage = () => {
         validate: vaildateSignin,
     });
 
-    const handleSubmit = () => {};
+    const handleSubmit = async () => {
+        console.log(values);
+        try {
+            const response = await postSignin(values);
+            setItem(response.data.accessToken);
+            console.log(response);
+        } catch (error) {
+            alert(error);
+        }
+    };
 
     // 이메일 또는 비밀번호가 비어있거나, 에러 메시지가 존재하면 버튼 비활성화
     const isDisabled = 
         Object.values(errors || {}).some((error) => error.length > 0) ||  // 오류가 있으면 true
         Object.values(values).some((value) => value === "");   // 빈 값이 있으면 true
 
+    const navigate = useNavigate();  // 뒤로 가기
+
     return (
-        <div className="flex flex-col items-center justify-center h-full gap-4">
-            <div className="flex flex-col gap-3">
-                <input
-                    {...getInputProps('email')}
-                    className={`border border-[#ccc] w-[300px] p-[10px] focus:border-[#807bff] rounded-sm
-                    ${errors?.email && touched?.email ? "border-red-500 bg-red-200" : "border-gray-300"}`}
-                    type={"email"}
-                    placeholder="이메일"
-                />
-                {errors?.email && touched?.email && (
-                    <div className="text-red-500 text-sm">{errors.email}</div>
-                )}
-                <input
-                    {...getInputProps('password')}
-                    className={`border border-[#ccc] w-[300px] p-[10px] focus:border-[#807bff] rounded-sm
-                    ${errors?.password && touched?.password ? "border-red-500 bg-red-200" : "border-gray-300"}`}
-                    type={"password"}
-                    placeholder="비밀번호"
-                />
-                {errors?.password && touched?.password && (
-                    <div className="text-red-500 text-sm">{errors.password}</div>
-                )}
-                <button 
-                    type="button" 
-                    onClick={handleSubmit} 
-                    disabled={isDisabled}
-                    className="w-full bg-blue-600 text-white py-3 rounded-md text-lg font-medium hover:bg-blue-700 transition-colors cursor-pointer disabled:bg-gray-300"
-                >
-                    로그인
+        <div className="flex flex-col items-center justify-center h-full gap-6">
+            <div className="flex items-center w-[300px] justify-between mb-4">
+                <button onClick={() => navigate(-1)} className="text-xl font-bold">
+                    &lt;
                 </button>
+                <h2 className="text-lg font-semibold">로그인</h2>
+                <div className="w-6" />
+            </div>
+
+            <div className="flex flex-col items-center gap-4">
+                <div className="flex flex-col gap-3">
+                    <input
+                        {...getInputProps('email')}
+                        className={`border border-[#ccc] w-[300px] p-[10px] focus:border-[#807bff] rounded-sm
+                        ${errors?.email && touched?.email ? "border-red-500 bg-red-200" : "border-gray-300"}`}
+                        type={"email"}
+                        placeholder="이메일"
+                    />
+                    {errors?.email && touched?.email && (
+                        <div className="text-red-500 text-sm">{errors.email}</div>
+                    )}
+                    <input
+                        {...getInputProps('password')}
+                        className={`border border-[#ccc] w-[300px] p-[10px] focus:border-[#807bff] rounded-sm
+                        ${errors?.password && touched?.password ? "border-red-500 bg-red-200" : "border-gray-300"}`}
+                        type={"password"}
+                        placeholder="비밀번호"
+                    />
+                    {errors?.password && touched?.password && (
+                        <div className="text-red-500 text-sm">{errors.password}</div>
+                    )}
+                    <button 
+                        type="button" 
+                        onClick={handleSubmit} 
+                        disabled={isDisabled}
+                        className="w-full bg-blue-600 text-white py-3 rounded-md text-lg font-medium hover:bg-blue-700 transition-colors cursor-pointer disabled:bg-gray-300"
+                    >
+                        로그인
+                    </button>
+                </div>
             </div>
         </div>
     );
