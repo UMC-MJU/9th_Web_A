70a916b1ab524d86640c302bac84cb4c15dd1d5c
eun <pdaeun0903@gmail.com>
2025-10-12 00:33:00 +0900

    FEAT 사용자 입력 유효성 검사 및 에러 메세지 표시

--- 은-박다은/4주차/mission2,3/src/pages/LoginPage.tsx
+++ 은-박다은/4주차/mission2,3/src/pages/LoginPage.tsx
@@ -1,23 +1,50 @@
+import useForm from "../hooks/useForm";
+import { validateSignin, type UserSigninInformation } from "../utils/validate";
+
 const LoginPage = () => {
-  const handleSubmit = () => {};
+  const { values, errors, touched, getInputProps } =
+    useForm<UserSigninInformation>({
+      initialValue: {
+        email: "",
+        password: "",
+      },
+      validate: validateSignin,
+    });
+
+  const handleSubmit = () => {
+    console.log(values);
+  };
   return (
     <div className="flex flex-col items-center justify-center h-full gap-4">
       <div className="flex flex-col gap-3">
         <input
-          className={
-            "border vorder-[#ccc] w-[300px] p-[10px] focus:border-[#807bff] rounded-sm"
-          }
+          {...getInputProps("email")}
+          className={`border border-[#ccc] w-[300px] p-[10px] focus:border-[#807bff] rounded-sm
+            ${
+              errors?.email && touched?.email
+                ? "border-red-500 bg-red-200"
+                : "border-gray-300"
+            }`}
           type={"email"}
           placeholder={"이메일"}
         />
-        <div className="text-red-500 text-sm">이메일 에러</div>
+        {errors?.email && touched?.email && (
+          <div className="text-red-500 text-sm">{errors.email}</div>
+        )}
         <input
-          className={
-            "border vorder-[#ccc] w-[300px] p-[10px] focus:border-[#807bff] rounded-sm"
-          }
+          {...getInputProps("password")}
+          className={`border border-[#ccc] w-[300px] p-[10px] focus:border-[#807bff] rounded-sm
+            ${
+              errors?.password && touched?.password
+                ? "border-red-500 bg-red-200"
+                : "border-gray-300"
+            }`}
           type={"password"}
           placeholder={"비밀번호"}
         />
+        {errors?.password && touched?.password && (
+          <div className="text-red-500 text-sm">{errors.password}</div>
+        )}
         <button
           type="button"
           onClick={handleSubmit}
