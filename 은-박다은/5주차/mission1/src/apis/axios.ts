5c2657a037a849ad63168f6876e72c030e71b12d
eun <pdaeun0903@gmail.com>
2025-10-30 14:46:57 +0900

    fix: axios 토큰 누락 문제 해결 및 인터셉터 추가

--- 은-박다은/5주차/mission1/src/apis/axios.ts
+++ 은-박다은/5주차/mission1/src/apis/axios.ts
@@ -3,9 +3,18 @@ import { LOCAL_STORAGE_KEY } from "../constants/key";
 
 export const axiosInstance = axios.create({
   baseURL: import.meta.env.VITE_SERVER_API_URL,
-  headers: {
-    Authorization: `Bearer ${localStorage.getItem(
-      LOCAL_STORAGE_KEY.accessToken
-    )}`,
-  },
 });
+
+axiosInstance.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem(LOCAL_STORAGE_KEY.accessToken);
+    const cleanToken = token ? token.replace(/"/g, "") : null;
+
+    if (cleanToken) {
+      config.headers.Authorization = `Bearer ${cleanToken}`;
+    }
+
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
