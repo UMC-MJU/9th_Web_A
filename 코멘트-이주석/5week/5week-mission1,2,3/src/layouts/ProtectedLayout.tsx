34f4d48c62700045c92551b62ec46c6f6155c071
Lee Ju Seok <sjmd117@gmail.com>
2025-10-31 16:25:34 +0900

    FEAT: Protected Route를 활용한 페이지 보호 기능 구현

---
+++ 코멘트-이주석/5week/5week-mission1,2,3/src/layouts/ProtectedLayout.tsx
@@ -0,0 +1,13 @@
+import { Navigate, Outlet, useLocation } from "react-router-dom";
+import { useAuth } from "../context/AuthContext";
+
+export const ProtectedLayout = () => {
+  const { accessToken } = useAuth();
+  const location = useLocation();
+
+  if (!accessToken) {
+    return <Navigate to={"/login"} state={{ location }} replace />;
+  }
+
+  return <Outlet />;
+};
