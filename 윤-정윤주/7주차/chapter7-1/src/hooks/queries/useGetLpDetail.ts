d70720e6266960911c1ebe40fcd923782f2f2d43
정윤주 <oyunjooya@naver.com>
2025-11-12 20:37:02 +0900

    Fix: useGetLpDetail에서 getLpDetail 호출 시 숫자를 객체로 수정

--- 윤-정윤주/7주차/chapter7-1/src/hooks/queries/useGetLpDetail.ts
+++ 윤-정윤주/7주차/chapter7-1/src/hooks/queries/useGetLpDetail.ts
@@ -2,10 +2,10 @@ import { useQuery } from "@tanstack/react-query";
 import { getLpDetail } from "../../apis/lp";
 import type { Lp } from "../../types/lp";
 
-export default function useGetLpDetail(lpid: number | string) {
+export default function useGetLpDetail(lpId: number | string) {
     return useQuery<Lp, Error>({
-        queryKey: ["lp", lpid],
-        queryFn: () => getLpDetail(Number(lpid)),
-        enabled: !!lpid,
+        queryKey: ["lp", lpId],
+        queryFn: () => getLpDetail({ lpId: Number(lpId) }),
+        enabled: !!lpId,
     });
 }
