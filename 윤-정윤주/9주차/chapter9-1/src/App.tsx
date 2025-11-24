7798da5864b1ad4f7e997331922c3decda192334
정윤주 <oyunjooya@naver.com>
2025-11-24 17:57:09 +0900

    Feat: 기본 Cart UI 구현

--- 윤-정윤주/9주차/chapter9-1/src/App.tsx
+++ 윤-정윤주/9주차/chapter9-1/src/App.tsx
@@ -1,9 +1,14 @@
 import './App.css'
+import CartList from './components/CartList'
+import Navbar from './components/Navbar'
 
 function App() {
 
   return (
-    <></>
+    <>
+      <Navbar />
+      <CartList />
+    </>
   )
 }
 
