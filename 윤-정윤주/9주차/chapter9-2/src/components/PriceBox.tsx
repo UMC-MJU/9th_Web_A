397ddf8f682537ed9587afa083953744bab6b805
정윤주 <oyunjooya@naver.com>
2025-11-25 16:19:32 +0900

    Chore: 9주차 미션1 프로젝트 복사

---
+++ 윤-정윤주/9주차/chapter9-2/src/components/PriceBox.tsx
@@ -0,0 +1,22 @@
+import { useDispatch, useSelector } from "../hooks/useCustomRedux";
+import { clearCart } from "../slices/cartSlice";
+
+const PriceBox = () => {
+    const { total } = useSelector((state) => state.cart);
+    const dispatch = useDispatch();
+
+    const handleInitializeCart = () => {
+        dispatch(clearCart());
+    };
+
+    return (
+        <div className="p-12 flex justify-between">
+            <button
+                onClick={handleInitializeCart} 
+                className="border p-4 rounded-md cursor-pointer">장바구니 비우기</button>
+            <div>총 가격: {total}\</div>
+        </div>
+    )
+};
+
+export default PriceBox;
