68252764264690401af030629b5059e69d6b666c
eun <pdaeun0903@gmail.com>
2025-11-26 19:44:11 +0900

    FEAT: Navbar · CartList · CartItem UI 구현 및 mock data 적용

---
+++ 은-박다은/9주차/mission1/src/components/CartList.tsx
@@ -0,0 +1,15 @@
+import cartItems from "../constants/cartItems";
+import CartItem from "./CartItem";
+
+const CartList = () => {
+  return (
+    <div className="flex flex-col items-center justify-center">
+      <ul>
+        {cartItems.map((item) => (
+          <CartItem key={item.id} lp={item} />
+        ))}
+      </ul>
+    </div>
+  );
+};
+export default CartList;
