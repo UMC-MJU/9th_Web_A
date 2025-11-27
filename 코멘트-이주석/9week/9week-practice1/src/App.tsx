cc1eee6544854c9da530324b22ee43c69b68ec91
Lee Ju Seok <sjmd117@gmail.com>
2025-11-27 10:30:06 +0900

    9주차실습1_코멘트

---
+++ 코멘트-이주석/9week/9week-practice1/src/App.tsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import UseReducerCompany from "./useReducerPage.tsx/UseReducerCompany";
+import UseReducerPage from "./useReducerPage.tsx/UseReducerPage";
+
+function App() {
+  return (
+    <>
+      <UseReducerPage />
+      <UseReducerCompany />
+    </>
+  );
+}
+
+export default App;
